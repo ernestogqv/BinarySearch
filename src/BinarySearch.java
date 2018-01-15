416704599dd34ec0fa068b133e5ec5c238897aea
ernestogqv <ernestogqv@gmail.com>
2018-01-15 00:10:43 -0500

    Update BinarySearch.java
    Now includes an algorithm that adds new values to the list and makes sure they are in order so that the binary search can be executed.

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,11 +1,12 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class BinarySearch {
 
     List<Integer> data;
 
-    public BinarySearch(List<Integer> list){
-        data = list;
+    public BinarySearch(){
+        data = new ArrayList<>();
     }
     public int search(int value){
         int length = ((data.size()) - 1);
@@ -27,5 +28,41 @@ public class BinarySearch {
             }
         }
         return location;
+    }
+    public void add(int value){
+        if(data.size() == 0){
+            data.add(value);
+        }else{
+            if(data.get(0) >= value){
+                data.add(0, value);
+            }else{
+                int length = ((data.size()) - 1);
+                if(data.get(length) <= value){
+                    data.add(value);
+                }else{
+                    int location = length/2;
+                    int location_checker = length/2;
+                    int piece_start = 0;
+                    boolean location_found = false;
+                    while (!location_found){
+                        if(data.get(location_checker) <= value && data.get(location_checker+1) >= value){
+                            location = location_checker;
+                            location_found = true;
+                            break;
+                        }
+                        if(data.get(location_checker) > value){
+                            length = location_checker;
+                        }
+                        else{
+                            if(data.get(location_checker) < value) {
+                                piece_start = location_checker;
+                            }
+                        }
+                        location_checker = piece_start + ((length - piece_start) / 2);
+                    }
+                    data.add(location+1, value);
+                }
+            }
+        }
     }
 }
