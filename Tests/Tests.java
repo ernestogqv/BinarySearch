b5eb935775d09497b763f07969ba3a95acc7285e
ernestogqv <ernestogqv@gmail.com>
2018-01-15 00:12:35 -0500

    Update Tests.java
    Updated test.

--- Tests/Tests.java
+++ Tests/Tests.java
@@ -1,25 +1,24 @@
 import org.junit.Test;
-import java.util.ArrayList;
-import java.util.List;
 import static org.junit.Assert.*;
 
 public class Tests {
 
     @Test
     public void testSearch(){
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        list.add(6);
-        list.add(7);
-        BinarySearch alpha = new BinarySearch(list);
-        assertEquals(alpha.search(3), 2);
+        BinarySearch alpha = new BinarySearch();
+        alpha.add(4);
+        alpha.add(1);
+        alpha.add(3);
+        alpha.add(7);
+        alpha.add(5);
+        alpha.add(6);
+        alpha.add(2);
         assertEquals(alpha.search(1), 0);
+        assertEquals(alpha.search(2), 1);
+        assertEquals(alpha.search(3), 2);
+        assertEquals(alpha.search(4), 3);
+        assertEquals(alpha.search(5), 4);
         assertEquals(alpha.search(6), 5);
         assertEquals(alpha.search(7), 6);
     }
 }
-
